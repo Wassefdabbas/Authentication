010cd5a8ae7718570807cec628ddcc438ae14c6b
Wassefdabbas <wassef.dabbas@gmail.com>
2025-09-30 23:48:51 +0300

    auth app

--- backend/mail/mail.js
+++ backend/mail/mail.js
@@ -1,21 +1,18 @@
-import {MailtrapClient} from "mailtrap";
+import { MailtrapClient } from "mailtrap";
 import 'dotenv/config'
 
 // ____NEW____
 import nodemailer from 'nodemailer'
 
-const TOKEN = process.env.MailToken;
-
 export const mailtrapClient = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 465,       // SSL port
+  secure: true,    // true for 465, false for 587
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: process.env.SMTP_PASS, // Must be an App Password from Google
   },
 });
-// export const mailtrapClient = new MailtrapClient({
-//   token: TOKEN,
-// });
 
 export const sender = {
   email: process.env.SMTP_USER,
