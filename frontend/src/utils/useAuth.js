72a4137bab15ef0a5b31ab60b0bfeac2db318c55
Wassefdabbas <wassef.dabbas@gmail.com>
2025-09-30 22:47:04 +0300

    Auth App - All file

--- frontend/src/utils/useAuth.js
+++ frontend/src/utils/useAuth.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 import 'dotenv/config'
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
     baseURL: API_URL,
